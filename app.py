e9f7ee3365bdb8e29ce238c80fb8ce8d327819a0
AMIT T <68996330+07AMIT10@users.noreply.github.com>
2024-10-19 14:19:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ try:
     vertexai.init(project=project_id, location="us-central1", credentials=credentials)
     
     # Initialize the Gemini model
-    model = GenerativeModel("gemini-pro-vision")
+    model = GenerativeModel("gemini-1.5-flash-002")
     st.success("Google Cloud credentials loaded and Gemini model initialized successfully")
 
     # Debug information
